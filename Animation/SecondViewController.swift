348b1cc73ab0cb62bd5a61aee2bd8e0ef39e38ef
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-11-12 22:07:15 +0400

    All changes

--- Animation/SecondViewController.swift
+++ Animation/SecondViewController.swift
@@ -4,9 +4,73 @@
 //
 //  Created by Mariam Joglidze on 12.11.23.
 //
-
 import UIKit
 
 class SecondViewController: UIViewController {
-
     
+    let gradient = CAGradientLayer()
+    var gradientSet = [[CGColor]]()
+    var currentGradient: Int = 0
+    
+    let gradientOne = UIColor(red: 48/255, green: 62/255, blue: 103/255, alpha: 1).cgColor
+    let gradientTwo = UIColor(red: 244/255, green: 88/255, blue: 53/255, alpha: 1).cgColor
+    let gradientThree = UIColor(red: 196/255, green: 70/255, blue: 107/255, alpha: 1).cgColor
+    
+    let label = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        gradientSet.append([gradientOne, gradientTwo])
+        gradientSet.append([gradientTwo, gradientThree])
+        gradientSet.append([gradientThree, gradientOne])
+        
+        gradient.frame = self.view.bounds
+        gradient.colors = gradientSet[currentGradient]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.drawsAsynchronously = true
+        self.view.layer.addSublayer(gradient)
+        
+        label.text = "TBC IT Academy"
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 20.0)
+        label.sizeToFit()
+        label.center = self.view.center
+        label.alpha = 0
+        self.view.addSubview(label)
+        
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseIn, animations: {
+            self.label.alpha = 1.0
+        }, completion: { _ in
+            self.moveLabel()
+        })
+        
+        animateGradient()
+    }
+    
+    func moveLabel() {
+        UIView.animate(withDuration: 4.0, delay: 0, options: [.autoreverse, .repeat], animations: {
+            self.label.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        }, completion: nil)
+    }
+    
+    func animateGradient() {
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 5.0
+        gradientChangeAnimation.toValue = gradientSet[currentGradient]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradient.add(gradientChangeAnimation, forKey: "colorChange")
+    }
+}
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -11,33 +11,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Set up the label
+
         label.text = "TBC IT Academy"
         label.sizeToFit()
         label.textColor = .red
         label.center = view.center
         view.addSubview(label)
 
-        // Call the function to start the animation
         startAnimation()
     }
 
     func startAnimation() {
-        // Set up the animation
         let animation = CAKeyframeAnimation(keyPath: "position")
 
-        // Define the path the label will follow
         let path = UIBezierPath()
         path.move(to: label.center)
-        path.addLine(to: CGPoint(x: label.center.x, y: label.center.y - 50)) // Adjust the distance as needed
+        path.addLine(to: CGPoint(x: label.center.x, y: label.center.y - 50))
         path.addLine(to: label.center)
 
-        // Set the animation properties
         animation.path = path.cgPath
         animation.duration = 5.0
         animation.repeatCount = .infinity
 
-        // Apply glitter effect during the animation
         let glitterEffect = CABasicAnimation(keyPath: "opacity")
         glitterEffect.fromValue = 1.0
         glitterEffect.toValue = 0.0
@@ -45,16 +40,13 @@ class ViewController: UIViewController {
         glitterEffect.repeatCount = .infinity
         glitterEffect.autoreverses = true
 
-        // Add the glitter effect to the label's layer
         label.layer.add(glitterEffect, forKey: "glitterEffect")
 
-        // Combine the position animation with the glitter effect
         let groupAnimation = CAAnimationGroup()
         groupAnimation.animations = [animation]
         groupAnimation.duration = 10.0
         groupAnimation.repeatCount = .infinity
 
-        // Add the animation to the label's layer
         label.layer.add(groupAnimation, forKey: "moveAndGlitter")
     }
 }
